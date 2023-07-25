3427bbc3eb8fa2446ed69945c65730ca8aff1b5c
Alexander Espinas <90251269+ft-Alexander@users.noreply.github.com>
2023-07-25 13:13:26 +0800

    Update smartContract.sol

--- Avalanche/smartContract.sol
+++ Avalanche/smartContract.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract smartContract {
+
+    address public contractOwner;
+
+    constructor() public {
+        contractOwner = msg.sender;
+    }
+
+    function checkRequire() public view returns(string memory){
+        require(msg.sender == contractOwner, "You are not the owner");
+        return "It's good";
+    }
+
+    function checkRevert() public view returns(string memory){
+        if(msg.sender != contractOwner){
+            revert("You are not the owner");
+        }
+        return "It's Good";
+    }
+
+    function checkAssert() public view {
+        assert(msg.sender == contractOwner);
+    }
+
+}
